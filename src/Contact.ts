333241f499c87dfc34c625c3ef4ed7506058b336
Savinien Barbotaud <savinienbarbotaud@gmail.com>
2025-12-28 11:49:49 +0100

    contact doc

--- src/Contact.ts
+++ src/Contact.ts
@@ -1,14 +1,27 @@
 import { ContactInfo } from "./utils";
 
+/**
+ * This class represent a Contact
+ */
 class Contact {
   id: string;
   name: string;
 
+  /**
+   * Create a contact
+   *
+   * @param id - ID of the contact
+   * @param name - Name of the contact
+   */
   constructor(id: string, name: string) {
     this.name = name;
     this.id = id;
   }
 
+  /**
+   *
+   * @returns The contact informations
+   */
   toString(): ContactInfo {
     return {
       id: this.id,
