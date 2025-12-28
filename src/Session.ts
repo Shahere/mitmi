7fdc6ce1ab7902dd9d6eca06c692d6a8135f50c4
Savinien Barbotaud <savinienbarbotaud@gmail.com>
2025-12-28 12:06:39 +0100

    docs session

--- src/Session.ts
+++ src/Session.ts
@@ -3,6 +3,11 @@ import { Conference } from "./Conference";
 import { SocketInteraction } from "./core/SocketInteraction";
 import { setCurrentSession } from "./utils";
 
+/**
+ * A session represent the local connection with the server
+ *
+ * @remarks Singleton
+ */
 class Session {
   name: string;
   socketInteraction: SocketInteraction;
@@ -10,12 +15,24 @@ class Session {
   private static session: Session;
   private static initializing?: Promise<Session>;
 
+  /**
+   * @private
+   * Create a session
+   *
+   * @param name - Your name
+   */
   private constructor(name: string) {
     this.name = name;
     this.socketInteraction = new SocketInteraction();
     this.contact = undefined;
   }
 
+  /**
+   * Create your session
+   *
+   * @param name - Your name
+   * @returns - Your Session
+   */
   static async create(name: string): Promise<Session> {
     // tout le tralala ici c'est pour eviter que React appel 2 fois create et cree 2 socket
     // Dans un cas de Singleton classique, Session est bien retourné mais pas la bonne car la socket et donc le contact ne sont pas bien initialisés
