f74576fe4b45ba58c4cefd9ea15279b094b02189
Savinien Barbotaud <savinienbarbotaud@gmail.com>
2025-12-29 20:22:33 +0100

    utils comms

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,11 @@
 import { Session } from "./Session";
 import { Stream } from "./Stream";
 
+/**
+ * Generate a random string
+ *
+ * @returns - A random string format, ex: de0b1c44-b42e-c617-bcc2-14586c5fffe2
+ */
 function uidGenerator(): String {
   var S4 = function () {
     return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
@@ -21,6 +26,9 @@ function uidGenerator(): String {
   );
 }
 
+/**
+ * Function for global session
+ */
 let currentSession: Session | undefined = undefined;
 function setCurrentSession(newSession: undefined | Session) {
   currentSession = newSession;
@@ -29,6 +37,10 @@ function getCurrentSession() {
   return currentSession;
 }
 
+//TODO Useless ?
+/**
+ * Function for localstream
+ */
 let localStream: Stream | undefined = undefined;
 function setLocalStream(newStream: undefined | Stream) {
   localStream = newStream;
@@ -37,6 +49,9 @@ function getLocalStream() {
   return localStream;
 }
 
+/**
+ * Contact informations
+ */
 interface ContactInfo {
   id: string;
   name: string;
